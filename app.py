248a8077adf041f2a0b0041d345f125518c30dff
Rares <rares.dan@rares-dan.ro>
2024-11-07 04:14:51 +0200

    environ.get("MY_USER")

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ CLOUDFLARE_ZONE_ID = os.environ.get("CLOUDFLARE_ZONE_ID")
 CLOUDFLARE_API_TOKEN = os.environ.get("CLOUDFLARE_API_TOKEN")
 PLACEHOLDER_EMAIL_DOMAIN = os.environ.get("PLACEHOLDER_EMAIL_DOMAIN")
 DESTINATION_EMAIL = os.environ.get("DESTINATION_EMAIL")
+MY_USER = os.environ.get("MY_USER")
 
 
 
@@ -21,7 +22,7 @@ ROUTING_URL = f"https://api.cloudflare.com/client/v4/zones/{CLOUDFLARE_ZONE_ID}/
 
 # Read user credentials from environment variables
 USER_CREDENTIALS = {
-    "admin": os.environ.get(
+    MY_USER: os.environ.get(
         "ADMIN_PASSWORD_HASH"
     )  # Set the encrypted password hash as an environment variable
 }
